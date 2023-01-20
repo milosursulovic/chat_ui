434c780dfc08693978e5b5a8f5a80ff537bb985d
Misa <milosursulovic2696@gmail.com>
2023-01-20 15:41:06 +0100

    Add chat message heart icon

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -14,15 +14,15 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe) {
-    return Container(
+    final Container msg = Container(
       margin: isMe
           ? const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 80.0)
           : const EdgeInsets.only(
               top: 8.0,
               bottom: 8.0,
-              right: 80.0,
             ),
       padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
       decoration: isMe
           ? BoxDecoration(
               color: Theme.of(context).colorScheme.secondary,
@@ -55,6 +55,29 @@ class _ChatScreenState extends State<ChatScreen> {
         ],
       ),
     );
+
+    if (isMe) {
+      return msg;
+    }
+    return Row(
+      children: [
+        msg,
+        IconButton(
+          icon: message.isLiked
+              ? const Icon(Icons.favorite)
+              : const Icon(Icons.favorite_border),
+          iconSize: 30.0,
+          color: message.isLiked
+              ? Theme.of(context).colorScheme.primary
+              : Colors.blueGrey,
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  _buildMessageComposer() {
+    return Text("message composer");
   }
 
   @override
@@ -101,6 +124,7 @@ class _ChatScreenState extends State<ChatScreen> {
               ),
             ),
           ),
+          _buildMessageComposer(),
         ],
       ),
     );
