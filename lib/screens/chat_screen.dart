989519c0bd11eed238298ddad3a6f849402a7c57
Misa <milosursulovic2696@gmail.com>
2023-01-20 15:32:10 +0100

    Style chat messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,6 +13,50 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  _buildMessage(Message message, bool isMe) {
+    return Container(
+      margin: isMe
+          ? const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 80.0)
+          : const EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              right: 80.0,
+            ),
+      padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      decoration: isMe
+          ? BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                bottomLeft: Radius.circular(15.0),
+              ))
+          : const BoxDecoration(
+              color: Color(0xFFFFEFEE),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0),
+              )),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message.time,
+            style: const TextStyle(
+                color: Colors.blueGrey,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: 8.0),
+          Text(message.text,
+              style: const TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600)),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,11 +85,20 @@ class _ChatScreenState extends State<ChatScreen> {
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30.0),
                       topRight: Radius.circular(30.0))),
-              child: ListView.builder(
-                  itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Text(messages[index].text);
-                  }),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)),
+                child: ListView.builder(
+                    reverse: true,
+                    padding: const EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    }),
+              ),
             ),
           ),
         ],
